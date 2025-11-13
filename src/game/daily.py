cbe2c8b8f4f244be8b2d3c61442e1ba7e975f677
Sam <smcgwithmc@gmail.com>
2025-11-13 12:45:48 -0800

    Created framework for end_game()

--- src/game/daily.py
+++ src/game/daily.py
@@ -1,4 +1,8 @@
+from datetime import date, timedelta
+
 from country import Country, get_country, get_random_country
+
+from phase2.statistics import RoundStatistics
 
 
 def get_daily_country() -> Country:
@@ -39,10 +43,26 @@ def compare_countries(guess: Country, answer: Country):
     pass
 
 
-def end_game(won: bool):
+def end_game(won: bool, mode: str = "daily"):
     """
     End the game in either a win or a loss, and pass this game's statistics
     on to be processed in statistics.py, and show a breakdown of this game's
     stats to the user
     """
-    pass
+
+    # TODO (milestone 2): Get the user id of the currently playing user, if there is one
+
+    # TODO: Get this round's stats properly
+    round_time: timedelta = timedelta()
+    round_guesses: int = 0
+
+    round_stats = RoundStatistics(
+        time_to_complete=round_time, won=won, guesses=round_guesses, mode=mode
+    )
+
+    if mode == "daily":
+        round_stats.daily_date = date.today()
+
+    # TODO: Add round to the rounds database
+
+    # TODO: Show game stats in UI
